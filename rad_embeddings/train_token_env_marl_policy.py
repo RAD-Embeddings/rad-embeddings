66cf4890ba22990edc1b009ac242e9f80bd140fd
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-07-22 17:43:06 -0700

    Uncomment wandb

--- rad_embeddings/train_token_env_marl_policy.py
+++ rad_embeddings/train_token_env_marl_policy.py
@@ -63,12 +63,12 @@ config = dict(
 )
 
 
-# run = wandb.init(
-#     entity="beyazit-y-berkeley-eecs",
-#     project="rad-marl",
-#     config=config,
-#     sync_tensorboard=True
-# )
+run = wandb.init(
+    entity="beyazit-y-berkeley-eecs",
+    project="rad-marl",
+    config=config,
+    sync_tensorboard=True
+)
 
 model = PPO(**config)
 
@@ -80,4 +80,4 @@ logger_callback = MarlLoggerCallback(gamma=config["gamma"])
 model.learn(10_000_000, callback=[logger_callback])
 model.save(f"exps_marl/token_env_marl_reach_avoid_policy_seed_{SEED}")
 
-# wandb.finish()
+wandb.finish()
