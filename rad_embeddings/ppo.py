f2803ce048fb9324b67f67b7d6dafdf7e1ecf8c1
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-08-20 15:49:41 -0700

    Print return dist in debug mode

--- rad_embeddings/ppo.py
+++ rad_embeddings/ppo.py
@@ -319,8 +319,8 @@ def make_train(config, env, network, batchify):
                     timesteps = info["timestep"][-1, :]
                     log["timestep"] = int(np.sum(timesteps) / config["NUM_AGENTS"])
 
-                    counts = Counter(return_buffer_debug)
-                    return_dist = {i: counts[i]/steps_per_update for i in counts}
+                    counts = Counter([int(x) for x in return_buffer_debug])
+                    return_dist = {i: float(counts[i])/float(steps_per_update) for i in counts}
 
                     jax.debug.print(
                         """
