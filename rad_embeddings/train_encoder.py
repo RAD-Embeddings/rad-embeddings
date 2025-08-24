3ffb7db72e08f5d41528ffa3e594b18a764f9296
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-08-24 16:43:43 -0700

    Try value_head in encoder

--- rad_embeddings/train_encoder.py
+++ rad_embeddings/train_encoder.py
@@ -25,6 +25,7 @@ class ActorCritic(nn.Module):
         # self.safe_l2_norm = lambda x: jnp.sum(x ** 2, axis=-1, keepdims=True)
         # self.safe_l2_norm = lambda x: max(jnp.linalg.norm(x, ord=2, axis=-1, keepdims=True), 1e-8) # https://github.com/jax-ml/jax/issues/3058
         self.policy_head = nn.Dense(self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0))
+        self.value_head = nn.Dense(1, kernel_init=orthogonal(1.0), bias_init=constant(0.0))
 
     # def safe_l2_norm(self, batch):
     #     batch_norm = jax.vmap(lambda x: jnp.array([jnp.maximum(jnp.linalg.norm(x, ord=2), jnp.float32(1e-8))]))(batch)
@@ -48,11 +49,14 @@ class ActorCritic(nn.Module):
         feat = self.encoder(graph)
         feat_l, feat_r = jnp.array_split(feat, 2)
 
-        feat_l_normalized = feat_l / self.safe_l2_norm(feat_l)
-        feat_r_normalized = feat_r / self.safe_l2_norm(feat_r)
-        value = self.safe_l2_norm(feat_l_normalized - feat_r_normalized)
+        # feat_l_normalized = feat_l / self.safe_l2_norm(feat_l)
+        # feat_r_normalized = feat_r / self.safe_l2_norm(feat_r)
+        # value = self.safe_l2_norm(feat_l_normalized - feat_r_normalized)
 
-        feat = jnp.concatenate([feat_l, feat_r, feat_l - feat_r], axis=-1)
+
+        feat = jnp.concatenate([feat_l, feat_r], axis=-1)
+
+        value = self.value_head(feat)
         logits = self.policy_head(feat)
 
         pi = distrax.Categorical(logits=logits)
