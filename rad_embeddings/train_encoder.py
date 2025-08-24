a91d2c572ada61416d98b9f8dc428bbb204aa6c2
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-08-24 13:06:01 -0700

    Working on safe_l2_norm

--- rad_embeddings/train_encoder.py
+++ rad_embeddings/train_encoder.py
@@ -25,10 +25,9 @@ class ActorCritic(nn.Module):
         # self.safe_l2_norm = lambda x: max(jnp.linalg.norm(x, ord=2, axis=-1, keepdims=True), 1e-8) # https://github.com/jax-ml/jax/issues/3058
         self.policy_head = nn.Dense(self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0))
 
-    def safe_l2_norm(self, x):
-        norm = jnp.linalg.norm(x, ord=2, axis=-1, keepdims=True)
-        norm = jnp.where(norm != 0, norm, 1e-8)
-        return norm
+    def safe_l2_norm(self, batch):
+        batch_norm = jax.vmap(lambda x: jnp.array([jnp.max(jnp.array([jnp.linalg.norm(x, ord=2), 1e-8]))]))(batch)
+        return batch_norm
 
     def __call__(self, batch):
         
