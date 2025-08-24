c2bebca1089da152d917d3fb511e625a50a927fa
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-08-24 16:40:08 -0700

    Add feat_l - feat_r to policy input

--- rad_embeddings/train_encoder.py
+++ rad_embeddings/train_encoder.py
@@ -52,7 +52,7 @@ class ActorCritic(nn.Module):
         feat_r_normalized = feat_r / self.safe_l2_norm(feat_r)
         value = self.safe_l2_norm(feat_l_normalized - feat_r_normalized)
 
-        feat = jnp.concatenate([feat_l, feat_r], axis=-1)
+        feat = jnp.concatenate([feat_l, feat_r, feat_l - feat_r], axis=-1)
         logits = self.policy_head(feat)
 
         pi = distrax.Categorical(logits=logits)
