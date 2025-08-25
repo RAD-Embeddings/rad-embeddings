92c65b24a11e9d13908ea99da4e09a6c8fa866ec
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-08-24 21:33:03 -0700

    Add safe_l2_norm back and use jnp.finfo(jnp.float32).eps

--- rad_embeddings/train_encoder.py
+++ rad_embeddings/train_encoder.py
@@ -21,7 +21,7 @@ class ActorCritic(nn.Module):
     encoder: nn.Module
 
     def setup(self):
-        # self.safe_l2_norm = lambda x: jnp.sqrt(jnp.sum(x ** 2, axis=-1, keepdims=True) + 1e-8)
+        self.safe_l2_norm = lambda x: jnp.sqrt(jnp.sum(x ** 2, axis=-1, keepdims=True) + jnp.finfo(jnp.float32).eps)
         self.policy_head = nn.Dense(self.action_dim, kernel_init=orthogonal(0.01), bias_init=constant(0.0))
 
     def __call__(self, batch):
@@ -42,12 +42,9 @@ class ActorCritic(nn.Module):
         feat = self.encoder(graph)
         feat_l, feat_r = jnp.array_split(feat, 2)
 
-        # feat_l_normalized = feat_l / self.safe_l2_norm(feat_l)
-        feat_l_normalized = feat_l / jnp.sqrt(jnp.sum(feat_l ** 2, axis=-1, keepdims=True) + 1e-8)
-        # feat_r_normalized = feat_r / self.safe_l2_norm(feat_r)
-        feat_r_normalized = feat_r / jnp.sqrt(jnp.sum(feat_r ** 2, axis=-1, keepdims=True) + 1e-8)
-        # value = self.safe_l2_norm(feat_l_normalized - feat_r_normalized)
-        value = jnp.sqrt(jnp.sum((feat_l_normalized - feat_r_normalized) ** 2, axis=-1, keepdims=True))
+        feat_l_normalized = feat_l / self.safe_l2_norm(feat_l)
+        feat_r_normalized = feat_r / self.safe_l2_norm(feat_r)
+        value = self.safe_l2_norm(feat_l_normalized - feat_r_normalized)
 
         logits = self.policy_head(feat_l - feat_r)
 
