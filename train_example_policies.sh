ea1bbbb8b64d3d100c17a6333857a01a414370ad
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-09-05 01:25:42 -0700

    Fix exp

--- train_example_policies.sh
+++ train_example_policies.sh
@@ -9,7 +9,7 @@ fi
 
 mkdir -p example_storage
 
-for seed in {0..4}; do
+for seed in {0..9}; do
   echo "Running seed $seed on GPU $DEVICE_ID..."
   CUDA_VISIBLE_DEVICES=$DEVICE_ID python rad_embeddings/train_policy.py \
     --seed $seed \
