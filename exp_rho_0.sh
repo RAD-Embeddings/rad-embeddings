c65e176b93c48ce67738d1cdfa97f1158c496c2c
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-09-10 16:51:45 -0700

    Add rho exp configs and script

--- exp_rho_0.sh
+++ exp_rho_0.sh
@@ -3,14 +3,14 @@
 mkdir -p exp_rho_storage
 
 for seed in {0..4}; do
-  CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.5.yaml &> out.txt
+  CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.5.yaml
 done
 
 for seed in {0..4}; do
-  CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_1.0.yaml &> out.txt
+  CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_1.0.yaml
 done
 
 for seed in {0..4}; do
-  CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.0.yaml &> out.txt
+  CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.0.yaml
 done
 
