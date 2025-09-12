cb971d6e09d5c5a05be9c65ab542714679fbead4
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-09-12 00:55:37 -0700

    Fix exps

--- exp_rho_0.sh
+++ exp_rho_0.sh
@@ -2,15 +2,12 @@
 
 mkdir -p exp_rho_storage
 
-for seed in {0..2}; do
+for seed in {0..1}; do
   CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.5.yaml
-done
-
-for seed in {0..2}; do
   CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_1.0.yaml
 done
 
-for seed in {0..2}; do
+for seed in {0..1}; do
   CUDA_VISIBLE_DEVICES=0 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.0.yaml
 done
 
