b8668d6028d48da4d238559048194714507dbe3f
beyazit-y <beyazit.yalcinkaya@gmail.com>
2025-09-12 13:51:23 -0700

    Add exps for decaying rho

--- exp_rho_1.sh
+++ exp_rho_1.sh
@@ -2,12 +2,16 @@
 
 mkdir -p exp_rho_storage
 
-for seed in {2..2}; do
-  CUDA_VISIBLE_DEVICES=1 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_1.0.yaml
-  CUDA_VISIBLE_DEVICES=1 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.5.yaml
-done
+# for seed in {2..2}; do
+#   CUDA_VISIBLE_DEVICES=1 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_1.0.yaml
+#   CUDA_VISIBLE_DEVICES=1 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.5.yaml
+# done
 
-for seed in {2..2}; do
-  CUDA_VISIBLE_DEVICES=1 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.0.yaml
+# for seed in {2..2}; do
+#   CUDA_VISIBLE_DEVICES=1 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho_0.0.yaml
+# done
+
+for seed in {0..2}; do
+  CUDA_VISIBLE_DEVICES=1 python rad_embeddings/train_policy.py --seed $seed --config config/exp_rho.yaml
 done
 
